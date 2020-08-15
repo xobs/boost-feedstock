4eb1d4ce708427cc563c30d13051058fef2ccbcb
Kai Tietz <ktietz@anaconda.com>
2020-08-15 16:41:35 +0200

    Update 1.71.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,3 +10,24 @@ xcopy /y user-config.jam %USERPROFILE%
 :: Start with bootstrap
 call bootstrap.bat
 if errorlevel 1 exit 1
+
+set CPU_COUNT=1
+
+:: Build step
+.\b2 install ^
+    --build-dir=buildboost ^
+    --prefix=%LIBRARY_PREFIX% ^
+    toolset=msvc-%VS_MAJOR%.0 ^
+    address-model=%ARCH% ^
+    variant=release ^
+    threading=multi ^
+    link=static,shared ^
+    --layout=system ^
+    --with-python ^
+    -j%CPU_COUNT%
+if errorlevel 1 exit 1
+
+:: Move dll's to LIBRARY_BIN
+move /y %LIBRARY_LIB%\boost*.dll "%LIBRARY_BIN%"
+:: if errorlevel 1 exit 1
+exit 0
--- recipe/install-py-boost.sh
+++ recipe/install-py-boost.sh
@@ -5,14 +5,14 @@ set -x -e
 # remove any old builds of the python target
 ./b2 -q -d+2 --with-python --clean
 
-for PY_VER2 in 3.6 3.7 3.8; do
+for PY_VER2 in 2.7 2.6 3.7 3.8; do
     ./b2 -q -d+2 \
         --with-python \
         python=${PY_VER} \
         --reconfigure \
         -j${CPU_COUNT} \
         cxxflags="${CXXFLAGS} -Wno-deprecated-declarations" \
-        clean 2>&1 | tee py-boost-%PY_VER%-clean.log
+        clean
 done
 
 ./b2 -q -d+2 \
@@ -21,7 +21,7 @@ done
     --reconfigure \
      -j${CPU_COUNT} \
      cxxflags="${CXXFLAGS} -Wno-deprecated-declarations" \
-     install 2>&1 | tee py-boost-%PY_VER%-install.log
+     install | tee b2.install-py-${PY_VER}.log 2>&1
 
 # boost.python, when driven via bjam always links to boost_python
 # instead of boost_python3. It also does not add any specified
