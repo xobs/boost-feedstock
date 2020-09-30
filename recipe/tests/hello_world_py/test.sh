1ee052106b802a535401339213c8e8aafd06337c
Ray Donnelly <mingw.android@gmail.com>
2020-09-30 15:14:15 +0200

    Test fixes

--- recipe/tests/hello_world_py/test.sh
+++ recipe/tests/hello_world_py/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-pushd libs/python/example/tutorial
+pushd ../../libs/python/example/tutorial
   export CONDA_BUILD_SYSROOT=/opt/MacOSX10.10.sdk
   export CONDA_BUILD=1
   bjam -q -d+2 --debug-configuration
